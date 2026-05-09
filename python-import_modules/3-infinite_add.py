6ecf9c2bfca4f02e480b3bffccd443c32d090329
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-05-09 23:42:04 +1000

    3-infinite_add.py  made exec, fixed syntax

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,4 +4,4 @@ accumulate = 0
 if __name__ == "__main__":
     for arg in sys.argv[1:]:
         accumulate = int(arg) + accumulate
-    print("{}".format(accumulate)
+    print("{}".format(accumulate))
