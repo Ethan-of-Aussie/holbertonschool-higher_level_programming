1d0c443c321e3c9b0a477c685800f84d66d63245
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-05-15 21:25:41 +1000

    6-print_sorted_dictionary.py stopped printing the last newline

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     diccopy = a_dictionary.copy()
-    print("\n".join(map(lambda k: f"{k}: {diccopy[k]}", sorted(diccopy))))
+    if diccopy:
+        print("\n".join(map(lambda k: f"{k}: {diccopy[k]}", sorted(diccopy))))
