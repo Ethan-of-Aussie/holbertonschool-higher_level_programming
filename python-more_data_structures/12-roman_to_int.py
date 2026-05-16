b0243cf3dbad5fd3710cb92f60c77519022dabd6
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-05-16 16:33:57 +1000

    12-roman_to_int.py roman numerial strings to int. subtract if next value bigger, add if smaller

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    romnum = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    total = 0
+    for key in range(len(roman_string)):
+        curchar = roman_string[key]
+        curval = romnum[curchar]
+
+        if key + 1 < len(roman_string):
+            nextchar = roman_string[key + 1]
+            nextval = romnum[nextchar]
+
+            if nextval > curval:
+                total -= curval
+            else:
+                total += curval
+        else:
+            total += curval
+    return total
