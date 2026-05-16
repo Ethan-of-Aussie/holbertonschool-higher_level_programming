e178b86e6bcab770b09c2a82b4eed2594ab66fc9
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-05-16 16:37:28 +1000

    12-roman_to_int.py added a check for None

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 def roman_to_int(roman_string):
     romnum = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     total = 0
+    if roman_string == None:
+        return 0
     for key in range(len(roman_string)):
         curchar = roman_string[key]
         curval = romnum[curchar]
