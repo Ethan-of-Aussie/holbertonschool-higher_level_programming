38c52fb625f477701e9adfdb94e601776547a76f
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-05-16 16:43:41 +1000

    12-roman_to_int.py changed check None to check for string only

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
+
 def roman_to_int(roman_string):
     romnum = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     total = 0
-    if roman_string == None:
+
+    if not isinstance(roman_string, str):
         return 0
     for key in range(len(roman_string)):
         curchar = roman_string[key]
