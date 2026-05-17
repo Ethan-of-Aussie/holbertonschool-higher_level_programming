dfd3613855280f6ff5d6328ea11ea8317e51e1a5
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-05-17 22:49:50 +1000

    0-add_integer.py added module and func descriptions

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+Module provides function that sums two integers.
+"""
+
 def add_integer(a, b=98):
+    """
+    Function sums up two intergers
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
