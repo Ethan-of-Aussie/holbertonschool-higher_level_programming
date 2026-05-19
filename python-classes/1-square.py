228e514c5701fc06edc919c78be3d08aea6e0e9f
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-05-19 13:37:16 +1000

    1-square.py added desc to file

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+Module to define size of square
+"""
 
 class Square:
+    """
+    Defines Square's size
+    """
+
     def __init__(self, size):
         self.__size = size
