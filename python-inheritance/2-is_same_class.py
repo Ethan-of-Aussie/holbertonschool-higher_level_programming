1308c0746efb78bf3aa2df6135377db003d7cf34
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-05-25 21:03:06 +1000

    2-is_same_class.py added docstring for module-level function

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -2,4 +2,5 @@
 """Module defines if object is same class."""
 
 def is_same_class(obj, a_class):
+    """Return True if obj is exactly an instance of a_class, otherwise False."""
     return type(obj) is a_class
