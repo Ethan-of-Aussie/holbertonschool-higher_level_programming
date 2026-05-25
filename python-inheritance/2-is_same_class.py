f644276ceb3333cec9cafd93513ce0479635d081
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-05-25 21:17:00 +1000

    2-is_same_class.py fixed up docstring

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """Module defines if object is same class."""
 
+
 def is_same_class(obj, a_class):
     """Return True if obj is exactly
     an instance of a_class, otherwise False."""
-
-
     return type(obj) is a_class
