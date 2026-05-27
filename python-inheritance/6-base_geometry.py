da711ddb94b7bc3dfb76cf6808e05d503af65411
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-05-27 14:18:36 +1000

    forgor to finish docstring

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Module defines class BaseGeometry"""
+"""Module defines class BaseGeometry."""
 
 
 class BaseGeometry:
-    """ """
+    """Class is not finished defining."""
 
     def area(self):
         raise Exception("area() is not implemented")
