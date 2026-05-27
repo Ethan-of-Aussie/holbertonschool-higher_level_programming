76bf5fcdcd13ae3198f0df37f312b333c478ed4e
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-05-27 14:39:49 +1000

    Validate value and raise area not finishes if called

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module defines class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Validates value, area not finished."""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
