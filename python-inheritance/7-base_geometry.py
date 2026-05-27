0fb38796b50f024681f4434f8ab1a502f7f4920c
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-05-27 15:26:07 +1000

    massive additons to test file and class module

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -4,7 +4,6 @@
 
 class BaseGeometry:
     """Validates value, area not finished."""
-
     def area(self):
         raise Exception("area() is not implemented")
 
@@ -13,13 +12,3 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        if isinstance(name, tuple) or isinstance(value, tuple):
-            raise TypeError("'name' and 'value' must not be a tuple")
-        if isinstance(name, list) or isinstance(value, list):
-            raise TypeError("'name' and 'value' must not be a list")
-        if isinstance(name, dict) or isinstance(value, dict):
-            raise TypeError("'name' and 'value' must not be a dictionary")
-        if value is None:
-            raise ValueError("must not be None")
-        if isinstance(value, bool):
-            raise ValueError("'value' must not be a boolen")
