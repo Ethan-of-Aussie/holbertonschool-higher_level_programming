2558ca776fe4789f8c16c3ad307642ec3aab8693
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-05-27 17:21:40 +1000

    blank lines

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -7,8 +7,6 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """Represents a square, inheriting from Rectangle."""
-
-
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
