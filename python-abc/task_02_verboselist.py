dae37ed44448a2e13b68948ee0cd348407213ad3
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-05-29 19:25:01 +1000

    task_02_verboselist.py extending builit in class list attributes

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+class VerboseList(list):
+
+    def append(self, item):
+        print("Added [{}] to the list.".format(item))
+        super().append(item)
+
+
+    def extend(self, item):
+        print("Extended the list with [{}] items.".format(len(item)))
+        super().extend(item)
+
+    def remove(self, item):
+        print("Removed [{}] from the list.".format(item))
+        super().remove(item)
+
+
+    def pop(self, item =- 1):
+        obj = super().pop(item)
+        print("Popped [{}] from the list.".format(obj))
+        return obj
