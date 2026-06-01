2ecb2307e57a3e80bdce2079736db4a3a5a6c7af
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-06-01 14:19:48 +1000

    0-read_file.py newline striped

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,6 @@
 def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout."""
     with open(filename, 'r', encoding="utf-8") as f:
-        data = f.read()
-        print(data)
+        for l in f:
+            l = l.rstrip('\n')
+            print(l)
