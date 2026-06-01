41c88b1e912d12bd73a08198221a13fc2682e58e
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-06-01 14:21:27 +1000

    0-read_file.py syntax fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout."""
     with open(filename, 'r', encoding="utf-8") as f:
-        for l in f:
-            line = l.rstrip('\n')
+        for line in f:
+            line = line.rstrip('\n')
             print(line)
