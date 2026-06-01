633ba5cff2ac7624d87bcec5eb953c0065cdcb43
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-06-01 14:10:46 +1000

    0-read_file.py docstrings

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
+"""Module defines read_file"""
 
 def read_file(filename=""):
+    """function that reads a text file (UTF8) and prints it to stdout."""
     with open(filename, 'r', encoding="utf-8") as f:
         data = f.read()
         print(data)
