73ab74b7bda8ffee9873a3506d496e183d88e3e7
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-06-03 15:31:55 +1000

    11-student.py replace values of attributes

---
+++ python-input_output/11-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines the Student class."""
+
+
+class Student:
+    """Represents a student with first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        return {key: val for key, val in self.__dict__.items() if key in attrs}
+
+    def reload_from_json(self, json):
+        for key, val in json.items():
+            setattr(self, key, val)
