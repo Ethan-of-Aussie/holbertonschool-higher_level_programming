75f324460e2cd8827659b4cb49fc7df97b81abc6
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-06-03 14:59:31 +1000

    10-student.py dictionary comprahension with __dict__ on json

---
+++ python-input_output/10-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines the Student class."""
+
+
+class Student:
+    """Represents a student with first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs == None:
+            return self.__dict__
+        return {key: val for key, val in self.__dict__.items() if key in attrs}
