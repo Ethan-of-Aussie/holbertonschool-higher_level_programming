5cb04078728ae9ea6269394b7c5200198250cbe9
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-06-03 13:20:02 +1000

    8-class_to_json.py serialized with __dict__ without importing json

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """Module defines class_to_json."""
 
-import json
-
 
 def class_to_json(obj):
     """JSON serialization of an object."""
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
