f525a4d9b07c42a362dae57bd0be04b0332e6042
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-06-07 13:20:44 +1000

    task_02_csv.py use DictReader to read rows of dictionary into a list. dump output to new file

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -15,5 +15,5 @@ def convert_csv_to_json(filename):
         with open("data.json", 'w', encoding="utf-8") as dj:
             return json.dump(data, dj)
         return True
-    except FileNotFoundError:
+    except Exception:
         return False
