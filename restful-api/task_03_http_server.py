3db38af34c45d37e1dfd70c3d74ea85c691ca4c6
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-06-11 16:26:35 +1000

    checks

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -21,7 +21,7 @@ class httpsub(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
-            self.wfile.write(b'{"OK"}')
+            self.wfile.write(b"OK")
             return
 
         elif self.path == "/":
