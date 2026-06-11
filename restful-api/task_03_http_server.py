76712425ad0b6235be2e909212f37a85ef45e470
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-06-11 16:23:30 +1000

    checks

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -14,7 +14,7 @@ class httpsub(http.server.BaseHTTPRequestHandler):
             self.end_headers()
 
             data = {"name": "John", "age": 30, "city": "New York"}
-            self.wfile.write(json.dumps(data.encode())
+            self.wfile.write(json.dumps(data).encode())
             return
 
         elif self.path == "/status":
