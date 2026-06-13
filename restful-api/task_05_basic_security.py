4c3802edd84a69d5c349c6c4582f697825dee652
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-06-13 19:01:27 +1000

    task_05_basic_security.py

---
+++ restful-api/task_05_basic_security.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+
+from flask import Flask, request, jsonify
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+app.config["JWT_SECRET_KEY"] = "super-secret"
+jwt = JWTManager(app)
+
+users = {
+    "user1":
+    {
+        "username": "user1",
+        "password": generate_password_hash("password"),
+        "role": "user"
+    },
+    "admin1":
+    {
+        "username": "admin1",
+         "password": generate_password_hash("password"),
+         "role": "admin"
+    }
+}
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and check_password_hash(users[username]["password"], password):
+        return username
+    return None
+
+
+@app.route('/basic-protected', methods=["GET"])
+@auth.login_required
+def index():
+    return "Basic Auth: Access Granted"
+
+
+@app.route('/login', methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+
+    if username not in users or not check_password_hash(users[username]["password"], password):
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return  jsonify(access_token=access_token)
+
+
+@app.route('/jwt-protected', methods=["GET"])
+@jwt_required()
+def jwtp():
+    current_user = get_jwt_identity()
+    return "JWT Auth: Access Granted"
+
+
+@app.route('/admin-only', methods=["GET"])
+@jwt_required()
+def adminonly():
+    current_user = get_jwt_identity()
+    if users[current_user]["role"] != "admin":
+        return {"error": "Admin access required"}, 403
+    return "Admin Access: Granted"
+
+if __name__ == '__main__':
+    app.run()
