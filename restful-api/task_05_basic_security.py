9863f7fd2d129408578c2d33327f0e55123df2c8
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-06-13 19:05:42 +1000

    task_05_basic_security.py

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -66,5 +66,21 @@ def adminonly():
         return {"error": "Admin access required"}, 403
     return "Admin Access: Granted"
 
+@jwt.unauthorized_loader
+def missing_token_callback(err):
+    return jsonify({"msg": "Missing authorization token"}), 401
+
+@jwt.invalid_token_loader
+def invalid_token_callback(err):
+    return jsonify({"msg": "Invalid token"}), 401
+
+@jwt.expired_token_loader
+def expired_token_callback(jwt_header, jwt_payload):
+    return jsonify({"msg": "Token has expired"}), 401
+
+@jwt.revoked_token_loader
+def revoked_token_callback(jwt_header, jwt_payload):
+    return jsonify({"msg": "Token has been revoked"}), 401
+
 if __name__ == '__main__':
     app.run()
