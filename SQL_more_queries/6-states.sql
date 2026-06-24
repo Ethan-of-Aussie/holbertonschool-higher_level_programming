9fda085495a6cdcdb6c6e5f81e6ec696b2176bfe
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-06-24 02:30:24 +0000

    6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,9 @@
 -- States table                                         
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
-AND IN hbtn_0d_usa CREATE TABLES IF NOT EXISTS states (
-    id INT NOT NULL UNIQUE PRIMARY,
+
+USE hbtn_0d_usa
+
+CREATE TABLES IF NOT EXISTS states (
+    id INT NOT NULL UNIQUE PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
