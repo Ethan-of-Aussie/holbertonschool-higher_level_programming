d3caf400636364da255bfcdc8442d0ab9b4d0fa2
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-06-26 16:55:59 +1000

    0-select_states.py

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Setting up arg entry for user to input password
+and gain access to database
+where we get all the states."""
+
+
+import MySQLdb, sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
