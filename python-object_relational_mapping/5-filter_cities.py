3aaa49f2bf89868993231ad854a5f67da2b6cea5
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-06-27 14:47:32 +1000

    5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
 
-    query = "SELECT cities.name \
-    FROM cities \
-    JOIN states ON cities.state_id = states.id \
-    WHERE states.name = %s \
-    ORDER BY cities.id ASC"
+    query = ("SELECT cities.name "
+    "FROM cities "
+    "JOIN states ON cities.state_id = states.id "
+    "WHERE states.name = %s "
+    "ORDER BY cities.id ASC")
 
     cur.execute(query, (sys.argv[4],))
 
