c5199251898be8de323dc3e9c145568bc2bbf34b
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-06-28 16:00:40 +1000

    9-model_state_filter_a.py

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine(
+        f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}",
+        pool_pre_ping=True)
+    session = sessionmaker(bind=engine)()
+
+    for state in session.query(State).order_by(State.id):
+        if 'a' in state.name.lower():
+            print(f"{state.id}: {state.name}")
+    session.close()
