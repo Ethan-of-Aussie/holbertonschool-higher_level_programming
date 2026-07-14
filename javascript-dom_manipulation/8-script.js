c3038484757289b4e9bd5994ecc500246cf690d2
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-07-14 17:19:20 +1000

    8-script.js

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('https://hellosalut.stefanbohacek.com/?lang=fr')
+    .then(response => response.json())
+    .then(data => {
+      const helloElement = document.querySelector('#hello');
+      helloElement.textContent = data.hello;
+    });
+});
